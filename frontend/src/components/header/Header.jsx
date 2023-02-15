dd99d8010d3f5043efc078f2fb36ae4ff7767677
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-02-15 22:37:06 +0100

    feat: :lipstick: update name and props

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -5,8 +5,10 @@ import style from './Header.module.css'
 const Header = () => {
   return (
     <div className={style.header} id="header">
-      <h1 className={style.title}>Project Title</h1>
-      <Button onClick={() => console.log('prova')}>Connect Wallet</Button>
+      <h1 className={style.title}>Liquid Finance</h1>
+      <Button onClick={() => console.log('prova')} connectWalletButton>
+        Connect Wallet
+      </Button>
     </div>
   )
 }
