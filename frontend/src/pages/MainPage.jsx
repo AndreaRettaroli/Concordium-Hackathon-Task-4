678c8d9d7040a1f35b1719ed644145f138560d4e
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-02-15 18:09:09 +0100

    feat: :construction: add main page

---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/header/Header'
+import Container from '../components/Container/Container'
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <Container />
+    </>
+  )
+}
+
+export default MainPage
