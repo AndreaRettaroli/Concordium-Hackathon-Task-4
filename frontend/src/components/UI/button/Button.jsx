e3da83204d9e4b0921102158462db10e4018a6cd
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-02-15 22:35:28 +0100

    feat: :art: customize button

--- frontend/src/components/UI/button/Button.jsx
+++ frontend/src/components/UI/button/Button.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import style from './Button.module.css'
 
 const Button = (props) => {
+  const className = props.connectWallet
+    ? style.button
+    : props.submitButton
+    ? style.formButton
+    : style.button
   return (
-    <button onClick={props.onClick} className={style.button}>
+    <button onClick={props.onClick} type={props.type} className={className}>
       {props.children}
     </button>
   )
