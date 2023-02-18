707f1fe10657488682fe499d177235553be9fd35
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-02-18 18:43:01 +0100

    feat: :zap: apply toggle in main page

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/header/Header'
 import DescriptionContainer from '../components/DescriptionContainer/DescriptionContainer'
 import StakeContainer from '../components/StakeContainer/StakeContainer'
+import UnstakeContainer from '../components/UnstakeContainer/UnstakeContainer'
+import Toggle from '../components/UI/Toggle/Toggle'
 
 const MainPage = () => {
   const isConnect = true
+  const [isStakeActive, setIsStakeActive] = useState(false)
   return (
     <>
       <Header />
+      <Toggle
+        isStakeActive={isStakeActive}
+        setIsStakeActive={setIsStakeActive}
+      />
       {!isConnect && <DescriptionContainer />}
-      {isConnect && <StakeContainer />}
+      {isConnect && isStakeActive && <StakeContainer />}
+      {isConnect && !isStakeActive && <UnstakeContainer />}
     </>
   )
 }
