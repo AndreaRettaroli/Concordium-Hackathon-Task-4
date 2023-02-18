de456bf23b82dc8479507b97aca50f7569e82f0c
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-02-18 21:36:54 +0100

    feat: :lipstick: update button styles

--- frontend/src/components/UI/button/Button.jsx
+++ frontend/src/components/UI/button/Button.jsx
@@ -6,7 +6,11 @@ const Button = (props) => {
     ? style.button
     : props.submitButton
     ? style.formButton
+    : props.connected
+    ? style.connected
     : style.formButton
+
+  console.log(props.connected)
   return (
     <button onClick={props.onClick} type={props.type} className={className}>
       {props.children}
