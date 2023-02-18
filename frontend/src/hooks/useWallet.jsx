4019b3ae507b5f3b7fb132b4568ae1785de54912
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-02-18 21:36:23 +0100

    feat: :construction: add wallet connection

---
+++ frontend/src/hooks/useWallet.jsx
@@ -0,0 +1,63 @@
+import {
+  AccountTransactionType,
+  CcdAmount,
+  ModuleReference,
+  serializeUpdateContractParameters,
+  toBuffer,
+} from '@concordium/web-sdk'
+
+import { detectConcordiumProvider } from '@concordium/browser-wallet-api-helpers'
+import Button from '../components/UI/button/Button'
+
+export async function init(setConnectedAccount) {
+  const client = await detectConcordiumProvider()
+  // Listen for relevant events from the wallet.
+  client.on('accountChanged', (account) => {
+    console.debug('browserwallet event: accountChange', { account })
+    setConnectedAccount(account)
+  })
+  client.on('accountDisconnected', () => {
+    console.debug('browserwallet event: accountDisconnected')
+    client.getMostRecentlySelectedAccount().then(setConnectedAccount)
+  })
+  client.on('chainChanged', (chain) => {
+    console.debug('browserwallet event: chainChanged', { chain })
+  })
+  client.getMostRecentlySelectedAccount().then(setConnectedAccount)
+
+  return client
+}
+
+export async function connect(client, setConnectedAccount) {
+  const account = await client.connect()
+  return setConnectedAccount(account)
+}
+
+export default function Wallet(props) {
+  const { client, connectedAccount, setConnectedAccount } = props
+  console.log(
+    '🚀 ~ file: useWallet.jsx:37 ~ Wallet ~ { client, connectedAccount, setConnectedAccount } ',
+    { client, connectedAccount, setConnectedAccount },
+  )
+  return (
+    <>
+      {!connectedAccount && (
+        <>
+          <Button
+            connectWalletButton
+            onClick={() =>
+              connect(client, setConnectedAccount).catch(console.error)
+            }
+          >
+            Connect Wallet
+          </Button>
+        </>
+      )}
+      {connectedAccount && (
+        <Button connected onClick={() => console.log('disconnect')}>
+          Disconnect <code>{connectedAccount.slice(0, 8).concat('.')}</code>.
+        </Button>
+      )}
+    </>
+  )
+}
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,23 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/header/Header'
 import DescriptionContainer from '../components/DescriptionContainer/DescriptionContainer'
 import StakeContainer from '../components/StakeContainer/StakeContainer'
 import UnstakeContainer from '../components/UnstakeContainer/UnstakeContainer'
 import Toggle from '../components/UI/Toggle/Toggle'
+import { init } from '../hooks/useWallet'
 
 const MainPage = () => {
-  const isConnect = true
-  const [isStakeActive, setIsStakeActive] = useState(false)
+  const [isStakeActive, setIsStakeActive] = useState(true)
+  const [client, setClient] = useState()
+  const [connectedAccount, setConnectedAccount] = useState()
+
+  // Attempt to initialize Browser Wallet Client.
+  useEffect(() => {
+    init(setConnectedAccount).then(setClient).catch(console.error)
+  }, [])
+  console.log(connectedAccount, client)
   return (
     <>
-      <Header />
-      <Toggle
-        isStakeActive={isStakeActive}
-        setIsStakeActive={setIsStakeActive}
+      <Header
+        setConnectedAccount={setConnectedAccount}
+        client={client}
+        connectedAccount={connectedAccount}
       />
-      {!isConnect && <DescriptionContainer />}
-      {isConnect && isStakeActive && <StakeContainer />}
-      {isConnect && !isStakeActive && <UnstakeContainer />}
+      {!connectedAccount ? (
+        <DescriptionContainer />
+      ) : (
+        <>
+          <Toggle
+            isStakeActive={isStakeActive}
+            setIsStakeActive={setIsStakeActive}
+          />
+          {isStakeActive ? <StakeContainer /> : <UnstakeContainer />}
+        </>
+      )}
     </>
   )
 }
