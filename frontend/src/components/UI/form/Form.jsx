8c799425c87956886e89c09651e340534698e4d4
Yury Katasonov <katasonov-yury@yandex.ru>
2023-02-25 17:56:26 +0300

    style form

--- frontend/src/components/UI/form/Form.jsx
+++ frontend/src/components/UI/form/Form.jsx
@@ -8,17 +8,26 @@ const Form = () => {
     handleSubmit,
     formState: { errors },
   } = useForm()
-  const onSubmit = (data) => console.log(data)
+  const onSubmit = (data) => console.log(data.amount)
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
       <input
         className={style.input}
-        defaultValue="0"
-        {...register('amount', { required: true })}
+        type="number"
+        placeholder="0.0"
+        {...register('amount', { required: true, min: 0, pattern: /^[+]?([.]\d+|\d+[.]?\d*)$/ })}
       />
-      {errors.amount && <span>This field is required</span>}
-      <Button type="submit" submitButton>
+      {errors.amount?.type === 'min' && <span className={style.error}>Only positive value</span>}
+      {errors.amount?.type === 'pattern' && <span className={style.error}>Enter a real number</span>}
+      {errors.amount?.type === 'required' && <span className={style.error}>This field is required</span>}
+
+
+      <Button
+        disabled={errors.amount}
+        type="submit"
+        submitButton
+      >
         Submit
       </Button>
     </form>
