369aad13232484fa7dc2fd9cbe1d5d4493d48463
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-02-25 20:30:24 +0100

    refactor: :lipstick: ui refactor

--- frontend/src/components/UI/form/Form.jsx
+++ frontend/src/components/UI/form/Form.jsx
@@ -16,17 +16,23 @@ const Form = () => {
         className={style.input}
         type="number"
         placeholder="0.0"
-        {...register('amount', { required: true, min: 0, pattern: /^[+]?([.]\d+|\d+[.]?\d*)$/ })}
+        {...register('amount', {
+          required: true,
+          min: 0,
+          pattern: /^[+]?([.]\d+|\d+[.]?\d*)$/,
+        })}
       />
-      {errors.amount?.type === 'min' && <span className={style.error}>Only positive value</span>}
-      {errors.amount?.type === 'pattern' && <span className={style.error}>Enter a real number</span>}
-      {errors.amount?.type === 'required' && <span className={style.error}>This field is required</span>}
+      {errors.amount?.type === 'min' && (
+        <span className={style.error}>Only positive value</span>
+      )}
+      {errors.amount?.type === 'pattern' && (
+        <span className={style.error}>Enter a real number</span>
+      )}
+      {errors.amount?.type === 'required' && (
+        <span className={style.error}>This field is required</span>
+      )}
 
-      <Button
-        disabled={errors.amount}
-        type="submit"
-        submitButton
-      >
+      <Button disabled={errors.amount} type="submit" submitButton>
         Submit
       </Button>
     </form>
