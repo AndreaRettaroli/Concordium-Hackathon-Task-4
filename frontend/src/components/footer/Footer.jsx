2bc8c5676d48c0a777562ebd67a421b7a55f9d37
Yury Katasonov <katasonov-yury@yandex.ru>
2023-02-25 18:25:54 +0300

    add target blank

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -9,27 +9,27 @@ const Footer = () => {
 
         <div className={style.col}>
           <h2 className={style.title}>Learn</h2>
-          <a href="https://concordium.com/articles-and-blogs/">Blog</a>
-          <a href="https://concordium.com/">Concordium</a>
-          <a href="https://developer.concordium.software/en/mainnet/index.html">Documentation</a>
+          <a target="_blank" href="https://concordium.com/articles-and-blogs/">Blog</a>
+          <a target="_blank" href="https://concordium.com/">Concordium</a>
+          <a target="_blank" href="https://developer.concordium.software/en/mainnet/index.html">Documentation</a>
         </div>
 
         <div className={style.col}>
           <h2 className={style.title}>Developers</h2>
-          <a href="https://github.com/Concordium/concordium-rust-sdk">Concordium Rust SDK</a>
-          <a href="https://github.com/Concordium/concordium-node-sdk-js">Concordium Javascript SDK</a>
+          <a target="_blank" href="https://github.com/Concordium/concordium-rust-sdk">Concordium Rust SDK</a>
+          <a target="_blank" href="https://github.com/Concordium/concordium-node-sdk-js">Concordium Javascript SDK</a>
         </div>
 
         <div className={style.col}>
           <h2 className={style.title}>Validators</h2>
-          <a href="https://developer.concordium.software/en/mainnet/net/nodes/node-requirements.html">Run node</a>
-          <a href="https://status.mainnet.concordium.software/">Status page</a>
-          <a href="https://support.concordium.software/c/releases/9">release information</a>
+          <a target="_blank" href="https://developer.concordium.software/en/mainnet/net/nodes/node-requirements.html">Run node</a>
+          <a target="_blank" href="https://status.mainnet.concordium.software/">Status page</a>
+          <a target="_blank" href="https://support.concordium.software/c/releases/9">release information</a>
         </div>
 
         <div className={style.col}>
           <h2 className={style.title}>Useful links</h2>
-          <a href="https://concordium.com/developer-ecosystem/">Developer ecosystem</a>
+          <a target="_blank" href="https://concordium.com/developer-ecosystem/">Developer ecosystem</a>
         </div>
 
       </div>
