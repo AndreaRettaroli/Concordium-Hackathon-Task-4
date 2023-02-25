a05b10e90af0749798b6c89fc3a931b43a2c43c7
andrea rettaroli <andrea.rettaroli@studio.unibo.it>
2023-02-25 19:55:01 +0100

    refactor: :recycle: change description

--- frontend/src/components/DescriptionContainer/DescriptionContainer.jsx
+++ frontend/src/components/DescriptionContainer/DescriptionContainer.jsx
@@ -12,7 +12,7 @@ const DescriptionContainer = () => {
           This is a Concordium Liquid Staking protocol. <br />
           Secure Concordium and earn rewards without your funds locked in.
           <br />
-          Get lqCCD, staked CCD supported all across the Concordium ecosystem.
+          Get wCCD, staked CCD supported all across the Concordium ecosystem.
         </p>
       </div>
     </Container>
